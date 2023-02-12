27e71ef3c8fc6a8cba900987afa37f8597761f4e
risarisato <nqg44226@gmail.com>
2023-02-12 16:50:42 +0900

    textareaのJS入力内容で高さを自動調整

--- textarea_test.js
+++ textarea_test.js
@@ -1,19 +1,16 @@
-let name = "あいうえおかきくけこさしすせそ";
-document.getElementById('name');
-
-
+//【JavaScript】textareaの入力内容で高さを自動調整する
 
 //textareaの要素取得
-let textarea = document.getElementById('textarea999');
+let textarea999 = document.getElementById("textarea999");
 //textareaのデフォルトの要素の高さを取得
-let ch = textarea.clientHeight;
+let ch = textarea999.clientHeight;
 
 //textareaのinputイベント
-textarea.addEventListener('input', ()=>{
+textarea999.addEventListener('input', ()=>{
     //textareaの高さを再設定（デフォルトの高さから計算するため）
-    textarea.style.height = ch + 'px';
+    textarea999.style.height = ch + 'px';
     //textareaの入力内容の高さを取得
-    let sh = textarea.scrollHeight;
+    let sh = textarea999.scrollHeight;
     //textareaの高さに入力内容の高さを設定
-    textarea.style.height = sh + 'px';
+    textarea999.style.height = sh + 'px';
 });
